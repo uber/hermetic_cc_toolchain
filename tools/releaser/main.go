f19c6bd2d53a5a022438e3da3ef4521aafc90727
Zhongpeng Lin <zplin@uber.com>
2023-09-04 11:39:44 -0700

    hardcode hash for v2.1.0

--- tools/releaser/main.go
+++ tools/releaser/main.go
@@ -41,6 +41,7 @@ var (
 	_tagHashes = map[string]string{
 		"v2.0.0-rc2": "40dff82816735e631e8bd51ede3af1c4ed1ad4646928ffb6a0e53e228e55738c",
 		"v2.0.0":     "57f03a6c29793e8add7bd64186fc8066d23b5ffd06fe9cc6b0b8c499914d3a65",
+		"v2.1.0":     "892b0dd7aa88c3504a8821e65c44fd22f32c16afab12d89e9942fff492720b37",
 	}
 
 	_boilerplateFiles = []string{
