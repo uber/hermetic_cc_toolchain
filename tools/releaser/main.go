3d1b4f1016943e01d87c2ff3665af342ed08ac42
Zhongpeng Lin <zplin@uber.com>
2023-09-24 17:11:57 -0700

    fix sha for v2.1.1

--- tools/releaser/main.go
+++ tools/releaser/main.go
@@ -42,6 +42,7 @@ var (
 		"v2.0.0-rc2": "40dff82816735e631e8bd51ede3af1c4ed1ad4646928ffb6a0e53e228e55738c",
 		"v2.0.0":     "57f03a6c29793e8add7bd64186fc8066d23b5ffd06fe9cc6b0b8c499914d3a65",
 		"v2.1.0":     "892b0dd7aa88c3504a8821e65c44fd22f32c16afab12d89e9942fff492720b37",
+		"v2.1.1":     "86ace5cd211d0ae49a729a11afb344843698b64464f2095a776c57ebbdf06698",
 	}
 
 	_boilerplateFiles = []string{
