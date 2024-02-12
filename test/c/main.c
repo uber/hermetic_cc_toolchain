4608b5431fe2e0f1714ca9b42e2f791d0f14d661
Motiejus Jakštys <motiejusja@wix.com>
2024-02-12 10:22:32 +0200

    Do not register MacOS toolchains
    Since `hermetic_cc_toolchain` does not support MacOS SDK, registering
    it as the default C++ toolchain causes a lot of headaches to run/skip
    the CGo tests.

--- test/c/main.c
+++ test/c/main.c
@@ -2,13 +2,22 @@
 // Licensed under the MIT License
 
 #include <stdio.h>
+#if defined(_WIN64)
+#define OS "windows"
+#elif __APPLE__
+#define OS "macos"
+#elif __linux__
+#define OS "linux"
 #include <features.h>
+#else
+#   error "Unknown compiler!"
+#endif
 
 int main() {
     #ifdef __GLIBC__
-    printf("glibc_%d.%d\n", __GLIBC__, __GLIBC_MINOR__);
+    printf("%s glibc_%d.%d\n", OS, __GLIBC__, __GLIBC_MINOR__);
     #else
-    printf("non-glibc\n");
+    printf("%s non-glibc\n", OS);
     #endif
     return 0;
 }
